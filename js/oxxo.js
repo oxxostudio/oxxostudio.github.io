00596c4fd6a7253a0b4e5084c042efc49064a988
oxxostudio <oxxo.studio@gmail.com>
2014-04-08 23:07:23 +0800

    test

--- js/oxxo.js
+++ js/oxxo.js
@@ -1,14 +1,4 @@
 $(function() {
-  var $window = $(window),
-    contentWidth = function() {
-      $('#content').css({
-        'width': ($window.width() - $('#sideNav').width() - $('#subMenu').width()) + 'px'
-      });
-      $('#content').text('width:' + $('#content').width() + 'px');
-    };
-  contentWidth();
-  $window.resize(contentWidth);
-
   $.getJSON('../pageList.json', function(data) {
   	var dataNum = data.length;
   	
