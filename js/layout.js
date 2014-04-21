72852789e742077f2856d19c554599c0d476f855
oxxostudio <oxxo.studio@gmail.com>
2014-04-22 00:07:05 +0800

    RWD layout

--- js/layout.js
+++ js/layout.js
@@ -1,13 +1,35 @@
 $(function() {
-  var $wrapper = $('#wrapper');
-  var $container = $('#container');
-  var containerWidth;
-  var $containerWidth = function() {
-    containerWidth = Math.round($wrapper.width()) - 230;
-    $container.css({
-      'width': containerWidth + 'px'
+    var $window = $(window);
+    var $body = $('body');
+    var $wrapper = $('#wrapper');
+    var $container = $('#container');
+    var $sideMenu = $('#sideMenu');
+    var fn_changeSize = function() {
+        if ($window.width() >= 1000) {
+            var containerWidth = $wrapper.width() - 230;
+            $container.css({
+                'width': containerWidth + 'px'
+            });
+            var containerHeight = $container.height();
+            $sideMenu.css({
+                'height': containerHeight + 50 + 'px'
+            });
+        } else {
+            $container.css({
+                'width': '100%'
+            });
+        }
+    }
+    fn_changeSize();
+    $(window).resize(fn_changeSize);
+
+    $('.menuList div').on('click', function() {
+        $('.menuList ul').css({'display':'table'});
+        $(this).find('span').css({'background':':#fff'});
     });
-  }
-  $containerWidth();
-  $(window).resize($containerWidth);
+    $container.on('click',function(){
+        $('.menuList ul').css({'display':'none'});
+        $('.menuList div span').css({'background':':#aaa'});
+    });
+
 });
