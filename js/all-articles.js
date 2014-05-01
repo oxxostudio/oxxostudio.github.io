265b686136b223593acb8c83eca62c3b3b2caffc
oxxostudio <oxxo.studio@gmail.com>
2014-05-01 23:58:16 +0800

    article layout

--- js/all-articles.js
+++ js/all-articles.js
@@ -1,12 +1,11 @@
 $(function() {
     var $window = $(window);
     var imgWidth, imgHeight, imgWidthMargin, imgHeightMargin;
-
     $.getJSON('../json/pageList.json', function(data) {
         var i;
         var dataNum = data.length;
-        for (i = 0; i < 6; i++) {
-            $('#container .new-articles .list ul').append(
+        for (i = 0; i < dataNum; i++) {
+            $('#container .article-list ul').append(
                 '<li url='+data[i].site+'>' +
                 '<div class="img-div">'+
                 '<img src="' + data[i].img + '">'+
@@ -15,16 +14,15 @@ $(function() {
                 '<span>' + data[i].date + '</span>' +
                 '</li>'
             );
-            if (i == 5) {
+            if (i == (dataNum-1)) {
                 fn_listHeight();
                 fn_imgHover();
                 fn_listLink();
             }
         }
     });
-
     var fn_imgHover = function() {
-        $('.new-articles .list ul li').hover(function() {
+        $('.article-list li').hover(function() {
             $(this).find('img').addClass('imghover').css({
                 'margin-top': -imgHeightMargin + 'px',
                 'margin-left': -imgWidthMargin + 'px'
@@ -38,19 +36,19 @@ $(function() {
     }
 
     var fn_listHeight = function() {
-        imgWidth = $('.new-articles .list .img-div').width();
+        imgWidth = $('.article-list .img-div').width();
         imgHeight = Math.round(imgWidth * 26 / 29);
         imgWidthMargin = imgWidth * 0.05;
         imgHeightMargin = imgHeight * 0.05;
-        $('.new-articles .list ul li .img-div').css({
+        $('.article-list .img-div').css({
             'height': imgHeight + 'px'
         });
     }
 
     var fn_listLink = function(){
-    	$('#container .new-articles .list ul li').on('click',function(){
-    		window.open($(this).attr('url'),'_self');
-    	});
+        $('.article-listl li').on('click',function(){
+            window.open($(this).attr('url'),'_self');
+        });
     }
 
     $window.resize(function() {
--- js/layout.js
+++ js/layout.js
@@ -4,16 +4,86 @@ $(function() {
     var $wrapper = $('#wrapper');
     var $container = $('#container');
     var $sideMenu = $('#sideMenu');
-    //RWD
+    var fn_sideMenuContent = function() {
+        $sideMenu.append(
+            '<div id="profile">' +
+            '<h1>OXXO.STUDIO</h1>' +
+            '<div class="img">' +
+            '<img src="img/layout/profile-img.png" height="74" width="74">' +
+            '</div>' +
+            '<div class="menu">' +
+            '<ul>' +
+            '<li>' +
+            '<i class="icon-sprite icon-menu icon-menu-aboutme"></i>' +
+            '<a href="#">About Me</a>' +
+            '</li>' +
+            '<li>' +
+            '<i class="icon-sprite icon-menu icon-menu-picasa"></i>' +
+            '<a href="#">Picasa</a>' +
+            '</li>' +
+            '<li>' +
+            '<i class="icon-sprite icon-menu icon-menu-contact"></i>' +
+            '<a href="#">Contact</a>' +
+            '</li>' +
+            '</ul>' +
+            '</div>' +
+            '</div>'
+        );
+        $sideMenu.append(
+            '<div class="line"></div>'
+        );
+        $sideMenu.append(
+            '<div class="menuList">' +
+            '<div><span></span><span></span><span></span></div>' +
+            '<ul>' +
+            '<li url="index"><a href="index.html">HOME</a></li>' +
+            '<li url="all-articles"><a href="all-articles.html">ALL ARTICLES</a></li>' +
+            '<li url="front-end"><a href="#">FRONT-END</a></li>' +
+            '<li url="css"><a href="#">CSS CRAFTS</a></li>' +
+            '<li url="ui"><a href="#">UI & UX</a></li>' +
+            '<li url="design"><a href="#">DESIGN</a></li>' +
+            '</ul>' +
+            '</div>'
+        );
+        $sideMenu.append(
+            '<div class="license">' +
+            '<span></span><span></span><span></span><span></span><span></span><span></span>' +
+            '<div>LICENSE</div>' +
+            '<div>This license lets others remix, tweak, ' +
+            'and build upon your work non-commercially, ' +
+            'as long as they credit you and license their ' +
+            'new creations under the identical terms.</div>' +
+            '</div>'
+        );
+        $container.prepend(
+            '<div class="container-title">' +
+            '<h1>' +
+            '<i class="icon-sprite icon-title"></i>' +
+            'OXXO.STUDIO<span> |&nbsp;&nbsp;Design thinking is everywhere</span>' +
+            '</h1>' +
+            '</div>' +
+            '<div class="line"></div>'
+        );
+        $('.menuList li[url=' + $('body').attr('url') + ']').find('a').addClass('active');
+    }
     var fn_changeSize = function() {
+        if ($container.height() < $window.height()) {
+            $container.css({
+                'height': $(window).height() + 'px'
+            });
+        }
         if ($window.width() >= 984) {
             var containerWidth = $wrapper.width() - 230;
             $container.css({
                 'width': containerWidth + 'px'
             });
             if ($('.menuList ul').hasClass('menuopen')) {} else {
-                $('.menuList ul').css({'opacity':'1'}).show().addClass('menuopen');
-                $('.menuList ul li').css({'opacity':'1'}).show();
+                $('.menuList ul').css({
+                    'opacity': '1'
+                }).show().addClass('menuopen');
+                $('.menuList ul li').css({
+                    'opacity': '1'
+                }).show();
             }
             $container.off('click');
         } else {
@@ -26,13 +96,33 @@ $(function() {
         }
     };
     var fn_menuListShow = function() {
-        $('.menuList ul').slideDown(200).animate({'opacity':'1'},{queue : false, duration : 200}).addClass('menuopen');
-        $('.menuList ul li').slideDown(200).animate({'opacity':'1'},{queue : false, duration : 200});
+        $('.menuList ul').slideDown(200).animate({
+            'opacity': '1'
+        }, {
+            queue: false,
+            duration: 200
+        }).addClass('menuopen');
+        $('.menuList ul li').slideDown(200).animate({
+            'opacity': '1'
+        }, {
+            queue: false,
+            duration: 200
+        });
         $('.menuList div span').addClass('span-hover');
     };
     var fn_menuListHide = function() {
-        $('.menuList ul').slideUp(200).animate({'opacity':'0'},{queue : false, duration : 200}).removeClass('menuopen');
-        $('.menuList ul li').slideUp(200).animate({'opacity':'0'},{queue : false, duration :200});
+        $('.menuList ul').slideUp(200).animate({
+            'opacity': '0'
+        }, {
+            queue: false,
+            duration: 200
+        }).removeClass('menuopen');
+        $('.menuList ul li').slideUp(200).animate({
+            'opacity': '0'
+        }, {
+            queue: false,
+            duration: 200
+        });
         $('.menuList div span').removeClass('span-hover');
     };
     var fn_sideMenuStatus = function() {
@@ -45,6 +135,7 @@ $(function() {
         });
         $container.on('click', fn_menuListHide);
     };
+    fn_sideMenuContent();
     fn_sideMenuStatus();
     fn_changeSize();
     $(window).resize(fn_changeSize);
