d1d2e83d631b043eb8a5b2aa17d6f3a6c8a89750
oxxostudio <oxxo.studio@gmail.com>
2014-05-18 00:30:51 +0800

    page num

--- js/index.js
+++ js/index.js
@@ -3,66 +3,81 @@ $(function() {
   var imgWidth, imgHeight, imgWidthMargin, imgHeightMargin,
     dataLength, urlPages, pageNum, pageTag;
   var nowUrl = location.href;
-  var urlParts = nowUrl.split("?");
-  if (urlParts[1]) {
-    urlPages = urlParts[1].split("=");
+  var urlParts1 = nowUrl.split("?");
+  var urlParts2 = nowUrl.split("/");
+  var siteUrl = urlParts2[0] + '//' + urlParts2[2] + '/';
+
+  if (urlParts1[1]) {
+    urlPages = urlParts1[1].split("=");
     pageTag = urlPages[0];
-    pageNum = urlPages[1];
+    pageNum = Number(urlPages[1]);
     if (urlPages[0] == 'tag-all') {
-      _showAll();
+      _showAll(1);
+      if (pageNum) {
+        _showAll(pageNum);
+      } else {
+        _showAll(1);
+      }
     } else {
-      _showTag(pageTag);
+      _showTag(pageTag, pageNum);
     }
   } else {
-    _showAll();
+    if (pageNum) {
+      _showAll(pageNum);
+    } else {
+      _showAll(1);
+    }
   }
 
-  function _showAll() {
+  function _showAll(nowPageNum) {
     $.getJSON('/json/pageList.json', function(data) {
       dataLength = data.length;
-      dataLength<6 ? maxNum=dataLength : maxNum=6;
+      dataLength < 6 ? maxNum = dataLength : maxNum = 6;
       var i = -1;
-      _renderContent(1);
+      _renderContent((i + nowPageNum) * 6);
 
       function _renderContent(Num) {
-        i = i + Num;
-        for (i = 0; i < 6; i++) {
+        var j, k;
+        k = Num + maxNum;
+        k < dataLength ? '' : k = dataLength;
+        for (j = Num; j < k; j++) {
           $('#content-grid>ul').append(
             '<li>' +
-            '<i class="' + data[i].tag + '"></i>' +
-            '<h2>' + data[i].title + '</h2>' +
-            '<h3><i class="icon-date"></i>' + data[i].date + '</h3>' +
+            '<i class="' + data[j].tag + '"></i>' +
+            '<h2>' + data[j].title + '</h2>' +
+            '<h3><i class="icon-date"></i>' + data[j].date + '</h3>' +
             '<h3><i class="icon-author"></i>OXXO.STUDIO</h3>' +
             '<div class="content-img">' +
             '<span></span>' +
-            '<img src="' + data[i].img + '">' +
+            '<img src="' + data[j].img + '">' +
             '</div>' +
             '<div class="content-grid-line"></div>' +
-            '<h4>' + data[i].desc + '</h4>' +
-            '<a href="' + data[i].site + '"><div class="read-more">Read more</div></a>' +
+            '<h4>' + data[j].desc + '</h4>' +
+            '<a href="' + data[j].site + '"><div class="read-more">Read more</div></a>' +
             '</li>'
           );
         }
       }
-      _pageNum(dataLength);
+      _allPageNum(dataLength, nowPageNum);
     });
   }
 
-  function _showTag(tagName) {
+  function _showTag(tagName, nowPageNum) {
     $.getJSON('/json/pageList.json', function(data) {
       dataLength = data.length;
       var classify = [];
       var classifyNum = 0;
-      var i,maxNum;
+      var i, maxNum;
       for (i = 0; i < dataLength; i++) {
         if (data[i].tag == tagName) {
           classify[classifyNum] = data[i];
           classifyNum = classifyNum + 1;
         }
       }
-      classifyNum<6 ? maxNum=classifyNum : maxNum=6;
+      classifyNum < 6 ? maxNum = classifyNum : maxNum = 6;
       var j = -1;
       _renderContent(1);
+
       function _renderContent(Num) {
         i = i + Num;
         for (j = 0; j < maxNum; j++) {
@@ -83,18 +98,35 @@ $(function() {
           );
         }
       }
+      _pageNum(classifyNum, nowPageNum);
     });
   }
 
-  function _pageNum(num) {
+  function _allPageNum(articleNum, nowPageNum) {
     var j, k;
-    if (num > 6) {
-      j = Math.ceil(num / 6);
-      for (k = 1; k <= j; k++) {
-        $('#page-num').append(
-          '<div>' + k + '</div>'
-        );
+    j = Math.ceil(articleNum / 6);
+    for (k = 1; k <= j; k++) {
+      $('#page-num').append(
+        '<div>' + k + '</div>'
+      );
+    }
+    $('#page-num div').on('click', function() {
+      var divIndex = $(this).index() + 1;
+      if (divIndex == 1) {
+        window.open(siteUrl, '_self');
+      } else {
+        window.open(siteUrl + 'index.html?=' + divIndex, '_self');
       }
+    });
+  }
+
+  function _pageNum(articleNum, nowPageNum) {
+    var j, k;
+    j = Math.ceil(articleNum / 6);
+    for (k = 1; k <= j; k++) {
+      $('#page-num').append(
+        '<div>' + k + '</div>'
+      );
     }
   }
 });
