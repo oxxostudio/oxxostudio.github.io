d66b12d316aa34cc4ae9c840284ebe7a23d84eac
oxxostudio <oxxo.studio@gmail.com>
2014-05-19 23:29:29 +0800

    add google analytics

--- js/layout.js
+++ js/layout.js
@@ -63,6 +63,16 @@ $(function() {
     '<div class="license">Copyright 2014 | All Rights Reserved. Designed by OXXO.STUDIO</div>' +
     '</div>'
   );
+  $body.append(
+    "<script>" +
+    "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
+    "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
+    "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
+    "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
+    "ga('create', 'UA-2708968-3', 'oxxostudio.github.io');" +
+    "ga('send', 'pageview');" +
+    "</script>"
+  );
 
   $window.scroll(function() {
     if ($window.scrollTop() > 160) {
